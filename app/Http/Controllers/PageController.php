00601fbfa3839ab7ca63ca4b3d420cf98a4bf844
Neizzzy <azat.azatch@yandex.ru>
2025-09-25 16:51:42 +0300

    Add PageController

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+        $tags = ['обучение', 'программирование', 'php', 'oop'];
+        return view('about', ['tags' => $tags]);
+    }
+}
