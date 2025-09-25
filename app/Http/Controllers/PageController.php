5b1fc37860039405a370cc96f10fce4f85f80fd3
Neizzzy <azat.azatch@yandex.ru>
2025-09-25 17:14:20 +0300

    Add home method

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+
+    public function home()
+    {
+        return view('home');
+    }
+
     public function about()
     {
         $tags = ['обучение', 'программирование', 'php', 'oop'];
