9036b4daf128c60eb911ce1b17c5f565c894febd
Neizzzy <azat.azatch@yandex.ru>
2025-09-29 13:23:17 +0300

    Add articles.show link

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -8,7 +8,9 @@
     <h2>Список статей</h2>
     @foreach($articles as $article)
         <div>
-            <h2>{{ $article->name }}</h2>
+            <a href="{{ route('articles.show', $article->id) }}">
+                <h3>{{ $article->name }}</h3>
+            </a>
             <div>
                 {{ Str::limit($article->body, 200) }}
             </div>
