2c701a2e378f1d91e922a080b644b03b31494a37
Neizzzy <azat.azatch@yandex.ru>
2025-09-29 18:25:13 +0300

    Update actions

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,14 +9,14 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::query()->paginate();
+        $articles = Article::paginate();
 
         return view('article.index', compact('articles'));
     }
 
     public function show($id)
     {
-        $article = Article::query()->findOrFail($id);
+        $article = Article::findOrFail($id);
 
         return view('article.show', compact('article'));
     }
