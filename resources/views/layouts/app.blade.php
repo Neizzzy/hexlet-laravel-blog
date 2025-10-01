b149c4556362bcceea502ee42bc1a328411b42f3
Neizzzy <azat.azatch@yandex.ru>
2025-10-01 13:58:28 +0300

    Add flash message and tailwind styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,22 +7,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Hexlet Blog - @yield('title')</title>
 </head>
 <body>
-    <header>
-        <nav class="navbar">
-            <div>
-                <a class="navbar-brand" href="{{ route('home') }}">Hexlet Blog</a>
+    <header class="py-4 bg-neutral-900">
+        <nav class="max-w-7xl mx-auto">
+            <div class="flex gap-10 items-center">
+                <a class="text-2xl font-bold color text-rose-500 hover:scale-102 duration-300" href="{{ route('home') }}">Hexlet Blog</a>
                 <div>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('about') }}">О нас</a>
+                    <ul class="flex gap-4">
+                        <li>
+                            <a class="text-white hover:text-rose-500 transition duration-300" href="{{ route('about') }}">О нас</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('articles.index') }}">Статьи</a>
+                            <a class="text-white hover:text-rose-500 transition duration-300" href="{{ route('articles.index') }}">Статьи</a>
                         </li>
                     </ul>
                 </div>
@@ -30,8 +29,16 @@
         </nav>
     </header>
 
-    <div class="container mt-4">
-        <h1>@yield('header')</h1>
+    @if(session('success'))
+        <div class="p-4 mb-4 text-sm text-green-800 rounded-b-lg bg-green-50" role="alert">
+            <div class="max-w-7xl mx-auto">
+                <span class="font-medium">{{ session('success') }}</span>
+            </div>
+        </div>
+    @endif
+
+    <div class="max-w-7xl mx-auto mt-4">
+        <h1 class="text-4xl font-bold text-gray-900">@yield('header')</h1>
         <div>
             @yield('content')
         </div>
